76ae96a67700d3963c1dfe9350601dbe7c8ef3d6
Danny <danny.gosain@hotmail.com>
2017-01-27 18:20:18 -0500

    added buttons and base code for graph

--- Assignment 1/js-page3.js
+++ Assignment 1/js-page3.js
@@ -72,3 +72,61 @@ for(var i = 0; i < keys.length; i++) {
   e.preventDefault();
   }
 }
+
+var canvas = document.getElementById('myCanvas'),
+    c = canvas.getContext('2d'),
+    
+    // 'n' is the number of line segments.
+    n = 100,
+    
+    // define the math "window".
+    xMin = -10,
+    xMax = 10,
+    yMin = -10,
+    yMax = 10,
+    
+    math = mathjs(),
+    expr = 'sin(x)*x',
+    scope = { x: 0 },
+    tree = math.parse(expr, scope);
+
+drawCurve();
+
+function drawCurve(){
+  // these are used inside the for loop.
+  var i, 
+      
+      // these vary between xMin and xMax
+      //                and yMin and yMax
+      xPixel, yPixel,
+      
+      // these vary between 0 and 1.
+      percentX, percentY,
+      
+      // these are in math coordinates.
+      mathX, mathY;
+  
+  c.beginPath();
+  for(i = 0; i < n; i++) {
+    percentX = i / (n - 1);
+    mathX = percentX * (xMax - xMin) + xMin;
+   
+    mathY = evaluateMathExpr(mathX);
+    
+    percentY = (mathY - yMin) / (yMax - yMin);
+    console.log(percentY);
+    
+    xPixel = percentX * canvas.width;
+    yPixel = percentY * canvas.height;
+    c.lineTo(xPixel, yPixel);
+  }
+  c.stroke();
+}
+
+function evaluateMathExpr(mathX){
+  scope.x = mathX;
+  console.log(tree.eval());
+  return tree.eval();
+}
+
+
